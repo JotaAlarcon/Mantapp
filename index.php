4d23f0632cd9fa3c33bd0dc59c424a875b62c7b5
Jhonnatan <alarcon.js@gmail.com>
2020-04-04 00:09:44 -0400

    se agregan templates y navbar a index

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+  include('templates/header.php');
+  include('templates/navbar.php');
+?>
+
+<div class="jumbotron">
+  <h1 class="display-4">Hello, world!</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
+<hr>
+<div class="card text-center">
+  <div class="card-header">
+    Mantapp
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  <div class="card-footer text-muted">
+    2 days ago
+  </div>
+</div>
+
+
+<?php
+include('templates/footer.php');
+?>
---
+++ templates/navbar.php
@@ -0,0 +1,24 @@
+<body>
+    <nav class="navbar navbar-light bg-danger navbar-expand-md">
+        <a href="index.php" class="navbar-brand">
+            <img src="img/bootstrap-solid.svg" alt="logo" width="30" height="30" class="d-inline-block align-top">
+        </a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01" >
+            <div class="navbar-nav mr-auto ml-auto text-center">
+                <a class="nav-item nav-link active" href="#">Inicio</a>
+                <a class="nav-item nav-link" href="#">Equipos</a>
+                <a class="nav-item nav-link" href="#">Usuarios</a>
+                <a class="nav-item nav-link" href="#">Contacto</a>
+            </div>
+            <div class="d-flex flex-row justify-content-center">
+                <a href="#" class="btn btn-warning mx-1">Acceder</a>
+                <a href="#" class="btn btn-outline-light mx-1">Registrar</a>
+            </div>
+        </div>
+
+    </nav>
